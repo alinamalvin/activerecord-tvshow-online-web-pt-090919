98f9a376c4c2c866595a612be73f78d9377395e6
Alina Kornilova <alinakornilova78@gmail.com>
2019-11-25 19:00:23 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,6 +5,10 @@ def self.highest_rating
   Song.maximum(:rating)
 end
 
- def self.most_popular_show
+def self.most_popular_show
     Show.where("rating = ?", self.highest_rating)[0]
-  end
+end
+  
+def self.lowest_rating
+  Song.minimum(:rating)
+end
